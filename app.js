817e2df8419fe723c97bdbf877fe49bfcfdafecb
bryanvengwa <bmvengwa@gmail.com>
2024-02-01 22:06:57 +0200

    implementation of the watch mode

--- app.js
+++ app.js
@@ -1,16 +1,1 @@
-function combine(n1, n2, conversionType) {
-    var result;
-    if (typeof n1 === 'number' && typeof n2 === 'number') {
-        result = n1 + n2;
-    }
-    else {
-        result = n1.toString() + n2.toString();
-    }
-    return result;
-}
-var combinedAges = combine(30, 40, 'as-number');
-var combinedStringAges = combine('30', '40', 'as-number');
-var combinedNames = combine('Martin', 'Luther', 'as-text');
-console.log(combinedAges);
-console.log(combinedStringAges);
-console.log(combinedNames);
+console.log('live server started');
