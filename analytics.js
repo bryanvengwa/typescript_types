73882d349b881dc0d23db458ab7a769ca22572af
bryanvengwa <bmvengwa@gmail.com>
2024-02-06 22:22:22 +0200

    removed some files from tsc

--- analytics.js
+++ analytics.js
@@ -1,2 +1,2 @@
 "use strict";
-console.log('sending...');
+console.log('sending... data');
--- functions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function add(n1, n2) {
-    return n1 + n2;
-}
-let combinedValues;
-combinedValues = add;
-function addAndHandle(n1, n2, cb) {
-}
