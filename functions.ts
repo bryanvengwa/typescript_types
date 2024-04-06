85d680346ac43794232cf24d09690d47e6da357e
bryanvengwa <bmvengwa@gmail.com>
2024-04-06 12:05:12 +0200

    the unknown type

---
+++ functions.ts
@@ -0,0 +1,14 @@
+function add(n1 : number, n2 : number) : number {
+    return n1 + n2;
+} 
+
+
+
+
+let combinedValues : (a: number, b: number)=> number;
+
+combinedValues = add ;
+
+function addAndHandle(n1 : number, n2 : number, cb : (a: number)=> void){
+
+}
