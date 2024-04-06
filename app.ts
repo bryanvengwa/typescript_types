21818d4a47c7d3265433808abdb966d0cbcaefb5
bryanvengwa <bmvengwa@gmail.com>
2024-04-06 11:13:14 +0200

    literal types

--- app.ts
+++ app.ts
@@ -1,4 +1,9 @@
-function combine(n1: number, n2: number) {
-  const result = n1 + n2;
+function combine(n1: number | string, n2: number | string) {
+  let result: number | string;
+  if (typeof n1 === 'number' && typeof n2 === 'number') {
+    result = n1 + n2;
+  } else {
+    result = n1.toString() + n2.toString();
+  }
   return result;
 }
