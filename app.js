9377254b9299568a664195f15f81cc98ae97bf2a
bryanvengwa <bmvengwa@gmail.com>
2024-04-06 11:19:53 +0200

    3 different implemantations of the combine function

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-var person = {
-    name: 'John',
-    age: 30,
-    hobbies: ['sports', 'cooking'],
-    role: [2, 'author'],
-};
-person.role.push('admin');
-console.log(person.role);
-console.log(person.age);
+function combine(n1, n2, conversionType) {
+    var result;
+    if (typeof n1 === 'number' && typeof n2 === 'number') {
+        result = n1 + n2;
+    }
+    else {
+        result = n1.toString() + n2.toString();
+    }
+    return result;
+}
+var combinedAges = combine(30, 40, 'as-number');
+var combinedStringAges = combine('30', '40', 'as-number');
+var combinedNames = combine('Martin', 'Luther', 'as-text');
+console.log(combinedAges);
+console.log(combinedStringAges);
+console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,4 +1,8 @@
-function combine(n1: number | string, n2: number | string) {
+function combine(
+  n1: number | string,
+  n2: number | string,
+  conversionType: string
+): string | number {
   let result: number | string;
   if (typeof n1 === 'number' && typeof n2 === 'number') {
     result = n1 + n2;
@@ -7,3 +11,11 @@ function combine(n1: number | string, n2: number | string) {
   }
   return result;
 }
+
+const combinedAges = combine(30, 40, 'as-number');
+const combinedStringAges = combine('30', '40', 'as-number');
+const combinedNames = combine('Martin', 'Luther', 'as-text');
+
+console.log(combinedAges);
+console.log(combinedStringAges);
+console.log(combinedNames);
