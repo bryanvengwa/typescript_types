d2b467fe83694df70c0d273b470bbd8ec9aab29b
bryanvengwa <bmvengwa@gmail.com>
2024-04-07 07:52:27 +0200

    implemented shorthand notation

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 "use strict";
 class Department {
-    constructor(name) {
+    constructor(id, name) {
+        this.employees = [];
         this.name = name;
+        this.id = id;
     }
     describe() {
         console.log('Department ' + this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
-const house = new Department('House');
+const house = new Department('1', 'House');
+house.addEmployee('bryan');
+house.addEmployee('Castle');
 console.log(house.describe());
-const copy = { describe: house.describe, name: 'copy' };
-console.log(copy.describe());
+console.log(house.printEmployeeInformation());
+// const copy = { describe: house.describe, name: 'copy' , employees:['fsf']: string[] };
+// console.log(copy.describe());
--- app.ts
+++ app.ts
@@ -1,16 +1,29 @@
 class Department {
+  id: string;
   name: string;
+  employees: string[] = [];
 
-  constructor(name: string) {
+  constructor(id: string, name: string) {
     this.name = name;
+    this.id = id;
   }
-  describe() {
+  describe(this: Department) {
     console.log('Department ' + this.name);
+  }
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const house = new Department('House');
+const house = new Department('1', 'House');
+house.addEmployee('bryan');
+house.addEmployee('Castle');
 console.log(house.describe());
-const copy = { describe: house.describe , name: 'copy' };
+console.log(house.printEmployeeInformation());
+// const copy = { describe: house.describe, name: 'copy' , employees:['fsf']: string[] };
 
-console.log(copy.describe());
+// console.log(copy.describe());
