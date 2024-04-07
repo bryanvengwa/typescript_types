6b69ecd8ce33767cd3e0c5f2e8de9e92fccf9652
bryanvengwa <bmvengwa@gmail.com>
2024-04-07 12:09:44 +0200

    intersection types

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
 "use strict";
-class person {
-    constructor(name) {
-        this.name = name;
-    }
-    greet(phrase) {
-        console.log(phrase + ': ' + this.name);
-    }
-}
-let user1;
-user1 = new person('Bryan');
-user1.greet('hi there');
