1674bb8c68587578bfde02df756e116c2854885d
bryanvengwa <bmvengwa@gmail.com>
2024-04-11 18:04:59 +0200

    initial drag and drop project

--- app.ts
+++ app.ts
@@ -1,19 +1,26 @@
-function LogClass(target: Function) {
-  console.log("Class name:", target.name);
-
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log('logging ....');
+  };
 }
-  
-@LogClass
+function withTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const  p = new constructor()
+    if (hookEl) {
+      hookEl.innerHTML = template; 
+      hookEl.querySelector('h1')!.innerHTML = p.name
+    }
+  };
+}
+
+// @Logger('LOGGING -PERSON')
+@withTemplate('<h1>My Person Object !!! </h1>', 'app')
 class Person {
-  name = "Max";
+  name = 'Max';
   constructor() {
-    console.log("Creating person object...");
-  }
-
-  greet() {
-    console.log("Hello from", this.name);
+    console.log('creating person object ...');
   }
 }
-
-const bryan = new Person();
-bryan.greet();
+const max = new Person();
+console.log(max);
